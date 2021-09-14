dcf7dc48614518b81e57c68a8f28badb6f36a3f5
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2021-09-15 01:37:32 +0200

    comments explaining exercise, styled components installed

--- src/App.js
+++ src/App.js
@@ -10,3 +10,25 @@ function App() {
 }
 
 export default App;
+
+
+// dupa123
+
+// [d,u,p,a,1,2,3] czyli ile inputów realnie ma hasło
+
+
+
+// [*,*,p,a,*,*,3]
+
+
+// const map = {
+//   2:'p',
+//   3:'a',
+//   6:'3'
+// }
+// [*,*,p,a,*,*,3]
+
+
+// ile inputów powienien widzieć człowiek od 7 do 14
+// [*,*,p,a,*,*,3] któ©e z liter użytkownik ma podać
+// [0,0,1,1,0,0,1,0,0,0,0,0,0,0,0] któ©e z inputów użytkownik ma odblokowane do wpisania
