1344757db0815228be4bfa0c4436b5e37f1bfb51
Kamil Szewczyk <kamilszewczyk0@gmail.com>
2021-09-25 01:21:49 +0200

    files refractor

--- src/Components/PasswordInput/PasswordInput.js
+++ src/Components/PasswordInput/PasswordInput.js
@@ -1,127 +1,28 @@
-import {
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  createRef,
-  useReducer,
-} from "react";
-import generatePasswordData from "../generatePasswordData/generatePasswordData";
-import SinglePasswordInput from "../../Components/SinglePasswordInput/SinglePasswordInput.js";
-import handlers from "../../helpers/handlers/handlers";
+import {useEffect} from "react";
+
+import useInputs from "../../hooks/useInputs";
 
 const PasswordInput = ({password, onSuccess}) => {
-  const [inputsLength, initialStateValues, correctValuesMap] = useMemo(
-    () => generatePasswordData(password),
-    [password],
-  );
-
-  const initialReducerValues = {
-    inputValues: {...initialStateValues},
-    inputsToIterate: [],
-    passwordVisible: false,
-    inputRefs: [],
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case "ON_CHANGE":
-        return {
-          ...state,
-          inputValues: {...state.inputValues, [action.field]: action.payload},
-        };
-      case "LOAD_INPUTS":
-        return {
-          ...state,
-          inputsToIterate: new Array(action.payload).fill(""),
-        };
-      case "SHOW_HIDE_PASSWORD":
-        return {
-          ...state,
-          passwordVisible: !state.passwordVisible,
-        };
-      case "SET_REFS":
-        return {
-          ...state,
-          inputRefs: action.payload,
-        };
-      default:
-        return state;
-    }
-  };
-
-  const [{inputValues, inputsToIterate, passwordVisible, inputRefs}, dispatch] =
-    useReducer(reducer, initialReducerValues);
-
-  const inputRef = useRef();
-
-  inputRef.current = inputsToIterate.map(
-    (_, index) => inputRef.current[index] ?? createRef(),
-  );
-
-  const checkInputsValues = useCallback(() => {
-    const passwordValues = Object.values(correctValuesMap);
-    const givenValues = Object.values(inputValues);
-
-    const ifAllInputsFilled = givenValues.every((item) => item && item.length);
-
-    const allInputsFilled = passwordValues.every(
-      (_, index) => passwordValues[index] === givenValues[index],
-    );
-
-    return ifAllInputsFilled ? onSuccess(allInputsFilled) : null;
-  }, [correctValuesMap, inputValues, onSuccess]);
-
-  const [handleChange, handleButtonClick, handleResetClick] = handlers(
+  const [
+    inputsLength,
+    inputsToIterate,
+    passwordVisible,
     dispatch,
-    inputRefs,
-    inputValues,
-  );
+    handleButtonClick,
+    handleResetClick,
+    checkInputsValues,
 
-  useEffect(() => {
-    const checkIfAllInputsHaveValues = Object.values(inputValues).every(
-      (char) => char && char.length !== 0,
-    );
-    if (checkIfAllInputsHaveValues) {
-      checkInputsValues();
-    }
-  }, [inputValues, checkInputsValues]);
-
-  useEffect(() => {
-    dispatch({type: "SET_REFS", payload: inputRef});
-  }, [inputRef]);
+    finalInput,
+  ] = useInputs(password, onSuccess);
 
   useEffect(() => {
     //montowanie komponenty=u
     dispatch({type: "LOAD_INPUTS", payload: inputsLength});
-  }, [inputsLength]);
+  }, [dispatch, inputsLength]);
 
   if (inputsToIterate.length === 0) {
     return null;
   }
-
-  const activeIndexesArray = Object.keys(correctValuesMap).map((item) =>
-    parseInt(item, 10),
-  );
-
-  const isActive = (inputIndex) =>
-    activeIndexesArray.some((index) => index === inputIndex);
-
-  const finalInput = inputsToIterate.map((_, index) => {
-    return (
-      <SinglePasswordInput
-        autoFocus={index === activeIndexesArray[0]}
-        ref={inputRef.current[index]}
-        maxLength="1"
-        key={index}
-        name={index}
-        disabled={!isActive(index)}
-        onChange={handleChange}
-        value={inputValues[index]}
-        type={passwordVisible ? "text" : "password"}
-      />
-    );
-  });
 
   return (
     <>
---
+++ src/hooks/useInputs.js
@@ -0,0 +1,126 @@
+import {createRef, useEffect, useMemo, useReducer, useRef} from "react";
+import generatePasswordData from "../Components/generatePasswordData/generatePasswordData";
+import SinglePasswordInput from "../Components/SinglePasswordInput/SinglePasswordInput";
+import handlers from "../helpers/handlers/handlers";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ON_CHANGE":
+      return {
+        ...state,
+        inputValues: {...state.inputValues, [action.field]: action.payload},
+      };
+    case "LOAD_INPUTS":
+      return {
+        ...state,
+        inputsToIterate: new Array(action.payload).fill(""),
+      };
+    case "SHOW_HIDE_PASSWORD":
+      return {
+        ...state,
+        passwordVisible: !state.passwordVisible,
+      };
+    case "SET_REFS":
+      return {
+        ...state,
+        inputRefs: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const checkIfAllInputsHaveValues = (values) =>
+  Object.values(values).every((char) => char && char.length !== 0);
+
+const checkInputsValues = (correctValues, providedValues, onSuccess) => {
+  const ifAllInputsFilled =
+    providedValues && providedValues.every((item) => item && item.length);
+
+  const allInputsFilled =
+    correctValues &&
+    correctValues.every(
+      (_, index) => correctValues[index] === providedValues[index],
+    );
+
+  return ifAllInputsFilled ? onSuccess(allInputsFilled) : null;
+};
+
+const isActive = (array, inputIndex) =>
+  array.some((index) => index === inputIndex);
+
+//  HOOK
+const useInputs = (password, onSuccess) => {
+  const [inputsLength, initialStateValues, correctValuesMap] = useMemo(
+    () => generatePasswordData(password),
+    [password],
+  );
+
+  const initialReducerValues = {
+    inputValues: {...initialStateValues},
+    inputsToIterate: [],
+    passwordVisible: false,
+    inputRefs: [],
+  };
+
+  const [{inputValues, inputsToIterate, passwordVisible, inputRefs}, dispatch] =
+    useReducer(reducer, initialReducerValues);
+
+  const [handleChange, handleButtonClick, handleResetClick] = handlers(
+    dispatch,
+    inputRefs,
+    inputValues,
+  );
+
+  const inputRef = useRef();
+
+  inputRef.current = inputsToIterate.map(
+    (_, index) => inputRef.current[index] ?? createRef(),
+  );
+
+  useEffect(() => {
+    if (checkIfAllInputsHaveValues(inputValues)) {
+      const passwordValues = Object.values(correctValuesMap);
+      const givenValues = Object.values(inputValues);
+
+      checkInputsValues(passwordValues, givenValues, onSuccess);
+    }
+  }, [inputValues, correctValuesMap, onSuccess]);
+
+  const activeIndexesArray = Object.keys(correctValuesMap).map((item) =>
+    parseInt(item, 10),
+  );
+
+  useEffect(() => {
+    dispatch({type: "SET_REFS", payload: inputRef});
+  }, [dispatch, inputRef]);
+
+  const finalInput = inputsToIterate.map((_, index) => {
+    return (
+      <SinglePasswordInput
+        autoFocus={index === activeIndexesArray[0]}
+        ref={inputRef.current[index]}
+        maxLength="1"
+        key={index}
+        name={index}
+        disabled={!isActive(activeIndexesArray, index)}
+        onChange={handleChange}
+        value={inputValues[index]}
+        type={passwordVisible ? "text" : "password"}
+      />
+    );
+  });
+
+  return [
+    inputsLength,
+    inputsToIterate,
+    passwordVisible,
+    dispatch,
+    handleButtonClick,
+    handleResetClick,
+    checkInputsValues,
+    finalInput,
+  ];
+};
+
+export default useInputs;
